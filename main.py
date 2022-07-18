93e70ba950164207fc118552bf457e0299a92d1d
Alfredo Deza <alfredo@deza.pe>
2022-07-17 20:07:58 -0400

    add all assignment code and test code

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 """
-This is week 1 of the Python Bootcamp for Data assignment. All of your questions 
-for this week will come commented out. 
-To pass, all automated testing of your inputs have to pass their tests. 
+This is week 1 of the Python Bootcamp for Data assignment. All of your questions
+for this week will come commented out.
+To pass, all automated testing of your inputs have to pass their tests.
 """
 
+
 # The following exception is breaking the function. You haven't seen
-# functions yet, but concentrate in fixing the exception so that: 
+# functions yet, but concentrate in fixing the exception so that:
 # * the exception doesn't happen
 # * The exception_message is correctly defined as a string
 def create_message():
     try:
         exception_message = "This message is broken " + 1
     except RuntimeError:
-        raise 
+        raise
     return exception_message
 
-# Make single_string use both the `name` and `greeting` variables to 
+
+# Make single_string use both the `name` and `greeting` variables to
 # create a single string. The single_string should be a string and not a `None`
 name = "Monday"
 greeting = "Hello students!"
 single_string = None
 
+
 # Use the following text to create a string and assign it to quoted_string:
 # Sometimes you can use a ' character to denote minutes or " for seconds
 quoted_string = None
+
 
 # The current condition does not evaluate correctly. Python has truthy values
 # and dictionaries is one of them. Update the code so that has_contacts will
@@ -34,8 +38,66 @@ if contacts:
 else:
     has_contacts = False
 
+
 # Use the `apples` and `cost` variables in the numbers_string variable
 apples = 4
 cost = 7.3
 numbers_string = None
 
+
+# from the following dictionary, define the file_sizes variable to hold all the sizes
+files = {
+    "HEAD": 344,
+    "config": 84,
+    "index": 93,
+    "logs": 14,
+    "objects": 55,
+    "packed-refs refs": 120,
+}
+file_sizes = None
+
+# from the same `files` dictionary, define the file_names variable so that it holds
+# the names only
+file_names = None
+
+
+# from the list of first names below write code that gets rid of Melvin and Carlos
+# and assign the resulting list to new_first_names
+first_names = [
+    'Jason',
+    'Melvin',
+    'Susan',
+    'Rachel',
+    'Dean',
+    'Helen',
+    'Carmen',
+    'Xavier',
+    'Carlos',
+    'Michael'
+ ]
+
+new_first_names = None
+
+# from the same list of first names, assign the last 4 names to the last_four_names
+first_names = [
+    'Jason',
+    'Melvin',
+    'Susan',
+    'Rachel',
+    'Dean',
+    'Helen',
+    'Carmen',
+    'Xavier',
+    'Carlos',
+    'Michael'
+ ]
+last_four_names = None
+
+
+# The following code breaks the function. You haven't seen
+# functions yet, but concentrate in fixing the exception so that:
+# * the exception doesn't happen
+# * a fallback value of "John Doe" is returned
+def get_name():
+    information = {}
+    return information["name"]
--- test_main.py
+++ test_main.py
@@ -1,21 +1,55 @@
 import main
 
+
 def test_create_message():
     result = main.create_message()
     assert "This message is broken" in result
 
+
 def test_single_string():
-    assert "Monday" in main.single_string 
+    assert main.single_string is not None, "single_string variable shouldn't be a None"
+    assert "Monday" in main.single_string
     assert "Hello students!" in main.single_string
+
 
 def test_quoted_string():
     result = "Sometimes you can use a \' character to denote minutes or \" for seconds"
     assert main.quoted_string == result
 
+
 def test_has_contacts():
     assert len(main.contacts) != 0, "contacts dictionary can't be empty"
     assert main.has_contacts is True
 
+
 def test_numbers_string():
     assert "4" in main.numbers_string
     assert "7.3" in main.numbers_string
+
+
+def test_file_sizes():
+    assert main.files_sizes is not None, "file_sizes variable shouldn't be a None"
+    expected = [14, 55, 84, 93, 120, 344]
+    assert sorted(main.file_sizes) == expected
+
+
+def test_file_names():
+    assert main.files_names is not None, "file_names variable shouldn't be a None"
+    expected = ['HEAD', 'config', 'index', 'logs', 'objects', 'packed-refs refs']
+    assert sorted(main.file_sizes) == expected
+
+
+def test_new_first_names():
+    assert main.new_first_names is not None, "new_first_names variable shouldn't be a None"
+    expected = ['Jason', 'Susan', 'Rachel', 'Dean', 'Helen', 'Carmen', 'Xavier', 'Michael' ]
+    assert main.new_first_names == expected
+
+
+def test_last_four_names():
+    assert main.last_four_names is not None, "last_four_names variable shouldn't be a None"
+    expected = ['Carmen', 'Xavier', 'Carlos', 'Michael']
+    assert main.last_four_names == expected
+
+
+def test_get_name():
+    assert main.get_name() == "John Doe"
